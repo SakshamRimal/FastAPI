634b179c6aa751b3b12b01c951d660f91a66f7b9
SakshamRimal <sakshamo345@gmail.com>
2025-10-19 10:39:00 +0545

    pydantic demo

--- pyantic_demo.py
+++ pyantic_demo.py
@@ -1,10 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel , EmailStr , Field
+from typing import List, Dict , Optional
 
 # pydantic model ma schema define garyo 
 class Patient(BaseModel):
-    name: str
-    age: int
+    name: str = Field(..., min_length=2 , max_length=50) # yo name ko lagi ho
+    age: int = Field(..., gt=0) # yo age ko lagi ho greater than 0
+    email: EmailStr # yo email ko lagi ho
+    weight: float = Field(..., gt=0 , lt=300) # yo weight ko lagi ho greater than 0 and less than 300
+    married: bool
+    allergies: Optional[List[str]] = None# list matra validate garnu xaina bhitra string xa ki nai ni garnu parxa so we use List 
+    contact_details: Dict[str , str] # in this too same 
       
+
 def insert_patient_data(patient: Patient):
     #yo duita data database ma rakhnu xa re
     print(patient.name)
@@ -17,7 +24,14 @@ def update_patient_data(patient: Patient):
     print(patient.age)
     
 
-Patient_info = {'name': 'shyam', 'age': 30}
+Patient_info = {'name': 'shyam', 'age': 30 ,
+                'email': 'abc@gmail.com' ,
+                'weight':72.2 , 
+                'married': True , 
+                'allergies':['pollen' , 'dust'] ,
+                'contact_details':{'phone': '223344'}
+}
+
 
 patient1 = Patient(**Patient_info) 
 
