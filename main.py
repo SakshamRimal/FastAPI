685f33bff510e3f416b96bd275553e2a55a5ebe0
SakshamRimal <sakshamo345@gmail.com>
2025-10-19 11:14:28 +0545

    Post method in FastAPI

--- main.py
+++ main.py
@@ -1,14 +1,53 @@
 
 from fastapi import FastAPI , Path , HTTPException , Query
+from pydantic import BaseModel , Field , computed_field
+from fastapi.responses import JSONResponse
+from typing import Annotated , Literal
 import json
 
 app = FastAPI()
+
+class Patient(BaseModel):
+    
+    id: Annotated[str , Field(... , description='id of the patient', examples=['P001'])]
+    name: Annotated[str , Field(..., description='Name of patient' , examples=['Ram'])]
+    city: Annotated[str , Field(...,description='city where the paitent is living')]
+    age : Annotated[int , Field(... , gt=0 , lt=120 , description='Age of patient')]
+    gender: Annotated[Literal['male' , 'female' , 'others'], Field(... , description='gender of the patient')]
+    height: Annotated[float , Field(...,gt=0 , description='height of the pateint')]
+    weight: Annotated[float , Field(... , gt=0 , description='Weight of the patient')]
+    
+    
+    # hamro feild exsiting field bata naya banauna milxa
+    @computed_field
+    @property 
+    def bmi(self) -> float:
+        bmi = round((self.weight/ self.height**2),2)
+        return bmi
+        
+    @computed_field
+    @property
+    def verdict(self) -> str:
+        if self.bmi < 18.5:
+            return 'UnderWeight'
+        elif self.bmi < 25:
+            return 'Normal'
+        elif self.bmi < 30:
+            return 'Normal'
+        else:
+            return 'Obesse'
+        
+        
 
 def load_data():
     with open("patients.json", "r") as file:
         data = json.load(file)
     return data
 
+def save_data(data):
+    with open('patient.json' , 'w') as file:
+        json.dump(data ,file)
+        
 @app.get("/")
 # route create gareko decorator ko help le 
 
@@ -50,10 +89,36 @@ def sort_patients(sort_by: str = Query(... , description='Sort on the basis of h
     if order not in ['asc' , 'desc']:
         raise HTTPException(status_code = 400, detail='invalid orider select between asc and desc')
     data = load_data()
-    sort_order = True if order=='desc' else False
+    sort_order = True if order == 'desc' else False
+    
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
+    
+    return sorted_data
+    
+# new endpoint named creatder=='desc' else False
     
     sorted_data = sorted(data.values() , key=lambda x :x.get(sort_by , 0 ), reverse=False)
     
     return sorted_data
     
+# new endpoint named create esma chai POST method use garne ani data pani pathaune so
+# client le server ma request post method use garera garxa then we validate the data using pydantic if the data is validated then we send the data.
+
+@app.post('/create')
+def create_patient(pateint: Patient):
     
+    # load the exsiting data 
+    data = load_data()
+    
+    # check if the pateint already exist or not 
+    if pateint.id in data:
+        raise HTTPException(status_code=400 , detail='Pateint already exist')
+
+    # if new patient add to the database 
+    data[pateint.id] = pateint.model_dump(exclude= ['id'])
+    
+    # save into the json file 
+    
+    save_data(data)
+    
+    return JSONResponse(status_code=201 , content={'messagee':'pateint created sucessfully'})
